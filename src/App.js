ca0d49024331727414a10add6bee06cc4b207a89
christopherau <chrisau168@gmail.com>
2025-07-16 20:45:06 +1000

    added container component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Button'
+import Navigation from './components/Navigation';
+import Button from './components/Button';
+import './components/Navigation.css'
+import Container from './components/Container'
 
 function App() {
+  
+  let navItemsList = [
+        { name: "Home", styling: "nav-styling"},
+        { name: "About", styling: "nav-styling"},
+        { name: "Services", styling: "nav-styling"},
+        { name: "Contact", styling: "nav-styling"}
+  ];  
+
+  console.log(navItemsList.length);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navigation styling="nav-container" navItemsList={navItemsList}/>     
       </header>
+      
+      <Container/>
+      
     </div>
   );
 }
